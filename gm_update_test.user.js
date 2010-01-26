37d7551810679958981ab08a2ce6fb08fe3a5855
Junya Ishihara <junya@champierre.com>
2010-01-26 21:28:03 +0900

    update the version

--- gm_update_test.user.js
+++ gm_update_test.user.js
@@ -4,7 +4,7 @@
 // @namespace      http://blog.champierre.com
 // @include        http://www.amazon.*
 // @require        http://svn.coderepos.org/share/lang/javascript/userscripts/GM_Libs/noriaki/UpdateChecker/UC-20080823.js  
-// @version        1.96
+// @version        1.97
 // ==/UserScript==
 
 alert('test');
@@ -12,6 +12,6 @@ alert('test');
 new UpdateChecker({  
   script_name: 'GM Update Test',
   script_url: 'http://github.com/champierre/gm_updater_test/raw/master/gm_update_test.user.js',
-  current_version: '1.96',
+  current_version: '1.97',
   more_info_url: 'http://libron.net'
 });
